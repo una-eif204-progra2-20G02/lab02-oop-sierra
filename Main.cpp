be5bd51258e7b67cd0758cd94a2624d514384c26
Wendy <Wendyaraya1234@gmail.com>
2020-08-07 12:49:08 -0600

    Main

--- Main.cpp
+++ Main.cpp
@@ -9,4 +9,16 @@
 
 int main() {
 
+    Professor *prof = new Professor("Mario", "Ruiz", 25685692, 300000, 0.05);
+    Administrative * adm1 = new Administrative ("Carlos", "Lopez", 35698720 , 500000);
+    University *uni;
+    std::vector<Professor> list;
+    list.push_back(*prof);
+    uni->setProfessorList(list);
+    std::vector<Administrative> list1;
+    list1.push_back(*adm1);
+    uni->setAdministrativeList(list1);
+
+    system("pause");
+    return 0;
 }
