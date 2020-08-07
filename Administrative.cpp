9d46ef8d1b3e8b7c218c9b46901c97606bc5e3c1
camisaenz510 <68917860+camisaenz510@users.noreply.github.com>
2020-08-07 11:45:35 -0600

    clase administrative

--- Administrative.cpp
+++ Administrative.cpp
@@ -5,3 +5,44 @@
 #include <sstream>
 #include <iomanip>
 #include "Administrative.h"
+
+Administrative::Administrative() {
+    this->monthlySalary=0.0;
+}
+
+Administrative::Administrative(double monthlySalary) {
+    this->monthlySalary=monthlySalary;
+}
+
+Administrative::Administrative(const std::string& firstName, const std::string& lastName, int documentId, double monthlySalary) : Person(firstName, lastName, documentId) {
+this->monthlySalary=monthlySalary;
+}
+
+double Administrative::getMonthlySalary() {
+    return this->getMonthlySalary();
+}
+
+void Administrative::setMonthlySalary(double monthlySalary) {
+    this->monthlySalary=monthlySalary;
+}
+
+const double Administrative::salary() {
+    return this->monthlySalary;
+}
+
+const std::string Administrative::toString() {
+    std::stringstream s;
+    s << "\tAdministrative Information: " << getFirstName() << getLastName() << std::endl;
+    s << "\tDoc ID:  " << getDocumentId() << ", " << "Monthly Salary: " << monthlySalary << std::endl;
+    return s.str();
+}
+
+
+
+
+
+
+
+
+
+
